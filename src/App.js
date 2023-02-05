d36ef52cb2093ac2651a854659d32b8f4f52483a
Sedanur Çevik <sdnrcvk@gmail.com>
2023-02-05 17:23:39 +0300

    AuthContext created

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import Booklist from "./components/BookList";
 import Navbar from "./components/Navbar";
 import ThemeToggle from "./components/ThemeToggle";
 import ThemeContextProvider, {ThemeContext} from "./contexts/ThemeContext";
+import AuthContextProvider from "./contexts/AuthContext";
 
 function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <Navbar/>
-        <Booklist/>
-        <ThemeToggle/>
-      </ThemeContextProvider>
+      <AuthContextProvider>
+        <ThemeContextProvider>
+          <Navbar/>
+          <Booklist/>
+          <ThemeToggle/>
+        </ThemeContextProvider>
+      </AuthContextProvider>
     </div>
   );
 }
